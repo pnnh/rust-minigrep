183dd4e01228c18ee2b8b360054e1910a5b3eec0
larry <xspanni@gmail.com>
2022-01-08 13:16:04 +0800

    完成命令行示例程序，实现搜索字符串，区分或不区分大小写

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
+
+use rust_minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    if args.len() < 3 {
-        println!("参数有误");
-        return;
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = rust_minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
     }
-
-    let query = &args[1];
-    let filename  = &args[2];
-
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
 }
