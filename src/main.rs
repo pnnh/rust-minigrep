1cd31fd0abc8a08ce3a641d38e8e458b5b781542
larry <xspanni@gmail.com>
2022-01-08 11:50:42 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    if args.len() < 3 {
+        println!("参数有误");
+        return;
+    }
+
+    let query = &args[1];
+    let filename  = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
